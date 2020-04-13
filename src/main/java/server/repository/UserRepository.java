93a4c8720b06e915828d985f47a9f2d55ec4dd0f
Julian Lu <j.lu@student.fontys.nl>
2020-04-13 12:32:11 +0200

    Authentication fixed with JWT

---
+++ src/main/java/server/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import server.entity.User;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUsernameAndPassword(String username,String password);
+
+    User findByUsername(String username);
+}
